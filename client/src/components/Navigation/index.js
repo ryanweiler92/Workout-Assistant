f8615fa9385b1b1af6f4868edd3761391c1d0a1d
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-23 10:19:51 -0400

    modal on navigation

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import {Container, Nav, Navbar, Col, Form, Button} from 'react-bootstrap'
+import {Container, Nav, Navbar, Modal, Tab} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import LoginForm from '../LoginForm/index'
+import SignupForm from '../SignupForm/index'
+import Auth from '../../utils/auth';
 
 const Navigation = () => {
 
@@ -12,18 +14,56 @@ const Navigation = () => {
         <>
         <Navbar expand='lg'>
             <Container fluid>
-                <Navbar.Brand >
+                <Navbar.Brand as={Link} to="/" >
                     Workout Assistant
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls='navbar' />
                 <Navbar.Collapse id='navbar'>
                 <Nav className="ml-auto">
+                    {Auth.loggedIn() ? (
+                    <>
                     <Nav.Link>Profile</Nav.Link>
-                    <Nav.Link>Login/Sign Up</Nav.Link>
+                    <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
+                    </>
+                    ) : (
+                    <Nav.Link onClick={() => setShowModal(true)}>Login/Sign Up</Nav.Link>
+                    )} 
                 </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+
+        <Modal
+         size="lg"
+         show={showModal}
+         onHide={() => setShowModal(false)}
+         aria-labelledby='signup-modal'>
+         <Tab.Container defaultActiveKey='login'>
+          <Modal.Header closeButton>
+            <Modal.Title id='signup-modal'>
+              <Nav variant='pills'>
+                <Nav.Item>
+                  <Nav.Link eventKey='login'>Login</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey='login'>
+                <LoginForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+              <Tab.Pane eventKey='signup'>
+                <SignupForm handleModalClose={() => setShowModal(false)} />
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
+        </Modal>
+
         <Container fluid className="hero">
         </Container>
         </>
---
+++ client/src/components/SignupForm/index.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+const SignupForm = () => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default SignupForm

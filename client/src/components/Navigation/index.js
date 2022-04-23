3b29237b4861bc34ae023d1165cddce8439da093
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-23 10:03:17 -0400

    modify navigation bar / create login form

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from 'react';
 import {Container, Nav, Navbar, Col, Form, Button} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import LoginForm from '../LoginForm/index'
 
 const Navigation = () => {
+
+    const [ showModal, setShowModal ] = useState(false);
 
 
     return (
@@ -12,12 +15,13 @@ const Navigation = () => {
                 <Navbar.Brand >
                     Workout Assistant
                 </Navbar.Brand>
+                <Navbar.Toggle aria-controls='navbar' />
+                <Navbar.Collapse id='navbar'>
                 <Nav className="ml-auto">
-                    <Nav.Link>Home</Nav.Link>
                     <Nav.Link>Profile</Nav.Link>
-                    <Nav.Link>Login</Nav.Link>
-                    <Nav.Link>Sign Up</Nav.Link>
+                    <Nav.Link>Login/Sign Up</Nav.Link>
                 </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
         <Container fluid className="hero">

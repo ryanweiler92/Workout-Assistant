469d824801e82d1876dd7b54c15d500e556fa6f7
ryanweiler92 <ryanweiler92@gmail.com>
2022-04-23 13:00:35 -0400

    login logic

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,3 +1,15 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password){
+      token
+      user {
+        username
+        email
+        _id
+      }
+    }
+  }
+`
 

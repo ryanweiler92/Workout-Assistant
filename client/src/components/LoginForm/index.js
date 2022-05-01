a1e6d0e17a6d003b1866a250ea1c5499443228e4
Kate Spitalnic <kespitalnic@gmail.com>
2022-05-01 00:00:11 -0400

    refactor code using react-bootstrap

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -4,17 +4,18 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
-const LoginForm = () => {
-    
+const LoginForm = (props) => {
+
     const [login, { error }] = useMutation(LOGIN_USER);
-    const [formState, setFormState] = useState({ email: '', password: ''});
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [showAlert, setShowAlert] = useState(false);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
 
-        setFormState({ 
-            ...formState, 
-            [name]: value, 
+        setFormState({
+            ...formState,
+            [name]: value,
         });
     };
 
@@ -22,99 +23,87 @@ const LoginForm = () => {
         event.preventDefault();
 
         try {
-            const { data } = await login({
-                variables: { ...formState }
+            const mutationResponse = await login({
+                variables: { email: formState.email, password: formState.password },
             });
-
-            Auth.login(data.login.token);
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
         } catch (e) {
-            console.error(e)
+            console.error(e);
+            setShowAlert(true);
         }
-
-        setFormState({
-            email: '',
-            password: ''
-        });
     };
-
     return (
-        <main className="flex-row justify-center mb-4">
-        <div className="col-12 col-md-6">
-          <div className="card">
-            <h4 className="card-header">Login</h4>
-            <div className="card-body">
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  id="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  id="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button className="btn d-block w-100" type="submit">
-                  Submit
-                </button>
-              </form>
-  
-              {error && <div>Login failed</div>}
-            </div>
-          </div>
-        </div>
-      </main>
+        <div className="container my-1">
+
+            <Form onSubmit={handleFormSubmit}>
+
+        <Form.Group>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Your email address'
+            name='email'
+            onChange={handleChange}
+            value={formState.email}
+            required
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleChange}
+            value={formState.password}
+            required
+          />
+        </Form.Group>
+                {/* <div className="flex-row space-between my-1">
+                    <label htmlFor="email">Email</label>
+                </div>
+                <div className="flex-row space-between my-2">
+                    <input
+                        placeholder="Your email address"
+                        name="email"
+                        type="email"
+                        id="email"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="flex-row space-between my-1">
+                    <label htmlFor="pwd">Password:</label>
+                </div>
+                <div className="flex-row space-between my-1">
+                    <input
+                        placeholder="Your password"
+                        name="password"
+                        type="password"
+                        id="pwd"
+                        onChange={handleChange}
+                    />
+                </div> */}
+                {error ? (
+                    <Alert dismissible onClose={() => setShowAlert(true)} show={showAlert} variant='danger'>
+                        Invalid login credentials!
+                    </Alert>
+                ) : null}
+                <div className="flex-row space-between my-3">
+                    <Button
+                        disabled={!(formState.email && formState.password)}
+                        type="submit"
+                        variant="success">
+                        Submit
+                    </Button>
+                </div>
+                </Form>
+
+            
+     </div>
+
     )
+}
 
-            {/* <Form noValidate validated={validated} onSubmit={handleFormSubmit} >
-                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-                    Invalid login credentials!
-                </Alert>
-                <Form.Group>
-                    <Form.Label htmlFor="email">Email</Form.Label>
-                    <Form.Control 
-                    type="text"
-                    placeholder="Your email"
-                    name="email"
-                    onChange={handleInputChange}
-                    value={userFormData.email}
-                    onSubmit={handleFormSubmit}
-                    required
-                    />
-                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label htmlFor="password">Password</Form.Label>
-                    <Form.Control
-                    type="password"
-                    placeholder="Your password"
-                    name="password"
-                    onChange={handleInputChange}
-                    value={userFormData.password}
-                    onSubmit={handleFormSubmit}
-                    required
-                    />
-                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-                </Form.Group>
-
-                <Button
-                disabled={!(userFormData.email && userFormData.password)}
-                type="submit"
-                variant="success">
-                Submit
-                </Button>
-            </Form> */}
-        // </>
-    // );
-};
 
 export default LoginForm;
